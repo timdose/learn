7dada0ff6fcc5c1610d98572d44c4b2fdee775a7
Tim Dose <timdose@gmail.com>
2024-10-25 13:44:48 -0400

    debugging form submission

--- workshops/process_waitlist.php
+++ workshops/process_waitlist.php
@@ -1,55 +1,52 @@
 <?php
-// Error reporting for debugging (remove in production)
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+// Disable error reporting for production
+error_reporting(0);
+ini_set('display_errors', 0);
 
-// Check if the request method is POST
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve and sanitize form data
-    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-    $itemName = filter_var($_POST['itemName'], FILTER_SANITIZE_STRING);
-    $price = filter_var($_POST['price'], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-    $originalPrice = filter_var($_POST['originalPrice'], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-    $discount = filter_var($_POST['discount'], FILTER_SANITIZE_NUMBER_INT);
-    $courseName = filter_var($_POST['courseName'], FILTER_SANITIZE_STRING);
+// Set the content type to JSON
+header('Content-Type: application/json');
 
-    // Validate email
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        http_response_code(400);
-        echo json_encode(["error" => "Invalid email address"]);
-        exit;
-    }
+// Log errors to a file instead of displaying them
+ini_set('log_errors', 1);
+ini_set('error_log', 'php_errors.log');
 
-    // Send email
-    $result = sendEmailNotification($email, $itemName, $price, $originalPrice, $discount);
+try {
+    // Check if the request method is POST
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Retrieve and sanitize form data
+        $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+        $itemName = htmlspecialchars($_POST['itemName'] ?? '', ENT_QUOTES, 'UTF-8');
+        $price = filter_var($_POST['price'] ?? '', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+        $originalPrice = filter_var($_POST['originalPrice'] ?? '', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+        $discount = filter_var($_POST['discount'] ?? '', FILTER_SANITIZE_NUMBER_INT);
 
-    if ($result) {
-        http_response_code(200);
-        echo json_encode(["message" => "Successfully added to waitlist"]);
+        // Validate email
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new Exception("Invalid email address");
+        }
+
+        // Send email
+        $to = "timdose@gmail.com";
+        $subject = "New Waitlist Submission";
+        $message = "New waitlist submission:\n\n";
+        $message .= "Email: $email\n";
+        $message .= "Item: $itemName\n";
+        $message .= "Price: $price\n";
+        $message .= "Original Price: $originalPrice\n";
+        $message .= "Discount: $discount%\n";
+
+        $headers = "From: waitlist@yourdomain.com\r\n";
+        $headers .= "Reply-To: $email\r\n";
+        $headers .= "X-Mailer: PHP/" . phpversion();
+
+        if (mail($to, $subject, $message, $headers)) {
+            echo json_encode(["message" => "Successfully added to waitlist"]);
+        } else {
+            throw new Exception("Failed to send email");
+        }
     } else {
-        http_response_code(500);
-        echo json_encode(["error" => "Failed to process request"]);
+        throw new Exception("Invalid request method");
     }
-} else {
-    http_response_code(405);
-    echo json_encode(["error" => "Method not allowed"]);
+} catch (Exception $e) {
+    echo json_encode(["error" => $e->getMessage()]);
 }
-
-function sendEmailNotification($email, $itemName, $price, $originalPrice, $discount) {
-    $to = "timdose@gmail.com";
-    $subject = "New Waitlist Submission for " . $courseName;
-    
-    $message = "New waitlist submission:\n\n";
-    $message .= "Email: $email\n";
-    $message .= "Item: $itemName\n";
-    $message .= "Price: $price\n";
-    $message .= "Original Price: $originalPrice\n";
-    $message .= "Discount: $discount%\n";
-
-    $headers = "From: tim@timdoseart.com\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "X-Mailer: PHP/" . phpversion();
-
-    return mail($to, $subject, $message, $headers);
-}
-?>
