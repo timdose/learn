9a694a71eac7edc7b8fe26f0cadca07c29ba1684
Tim Dose <timdose@gmail.com>
2024-11-02 16:04:21 -0400

    build & deploy: 2024-11-02 16:04:21

--- _build/workshops/process_time_preference.php
+++ _build/workshops/process_time_preference.php
@@ -19,7 +19,9 @@ try {
     $to = 'timdose@gmail.com';
     $subject = 'New Workshop Time Preference Submission';
     $message = "Email: $email\n";
-    $message .= "Preferred Times: " . implode(", ", $timePreference) . "\n";
+    $message .= "Preferred Times:\n" . implode("\n - ", array_map(function($time) {
+        return trim($time);
+    }, $timePreference)) . "\n";
     if ($otherTimes) {
         $message .= "Other Times: $otherTimes\n";
     }
