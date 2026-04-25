cb797e023ffbea635653269c186cc48771c9def5
Tim Dose <timdose@gmail.com>
2026-04-24 20:53:41 -0400

    moved old static folders to public/ and cleaned up a bunch of old files for jekyll that aren't needed anymore

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,30 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import yaml from '@rollup/plugin-yaml';
+import { existsSync } from 'fs';
+import { join } from 'path';
+
+function servePublicDirectoryIndexes() {
+  return {
+    name: 'serve-public-directory-indexes',
+    configureServer(server) {
+      server.middlewares.use((req, _res, next) => {
+        const url = req.url?.split('?')[0].split('#')[0] ?? '';
+        const normalized = url.endsWith('/') ? url : url + '/';
+        const indexPath = join(process.cwd(), 'public', normalized, 'index.html');
+        if (existsSync(indexPath)) req.url = normalized + 'index.html';
+        next();
+      });
+    },
+  };
+}
 
 export default defineConfig({
   site: 'https://learn.timdoseart.com',
   integrations: [mdx()],
   outDir: '_build',
   vite: {
-    plugins: [yaml()],
+    plugins: [yaml(), servePublicDirectoryIndexes()],
     optimizeDeps: {
       exclude: ['workshops'],
       entries: ['src/**/*.{astro,mdx}'],
